299966fa70e1a53c2eddc2c91f2d3475f199bfd8
UlianaOr <Ouliana9@gmail.com>
2022-02-23 17:41:33 +0100

    qwe

--- js/game.js
+++ js/game.js
@@ -1,2 +1,16 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getCotext("2d");
+
+const ground = new Image();
+ground.src = "img/ground.png";
+
+const food = new Image();
+food.src = "img/food.png";
+
+let box = 32;
+
+let score = 0;
+
+function drowGame() {
+    ctx.drawImage(ground, 0, 0);
+}
