60e7e4c5869087c2e90dd553c759ffbd7cc44188
UlianaOr <Ouliana9@gmail.com>
2022-02-24 16:04:31 +0100

    qwew

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById("game");
-const ctx = canvas.getCotext("2d");
+const ctx = canvas.getContext("2d");
 
 const ground = new Image();
 ground.src = "img/ground.png";
@@ -11,6 +11,12 @@ let box = 32;
 
 let score = 0;
 
-function drowGame() {
+let food = {
+    x: Math.floor((Math.random() * 17 + 1)) * box,
+    y: Math.floor((Math.random() * 15 + 1)) * box,
+};
+
+function drawGame() {
     ctx.drawImage(ground, 0, 0);
 }
+let game = setInterval(drawGame, 100);
